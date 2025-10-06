69a38045a62ddb2c6ebc3a82b97425d3dee0d17a
Sohaib <somar54@hotmail.fr>
2025-10-06 23:59:39 +0200

    ajout comm

--- tests/Security/GithubUserProviderTest.php
+++ tests/Security/GithubUserProviderTest.php
@@ -66,7 +66,7 @@ class GithubUserProviderTest extends TestCase {
     public function tearDown() : void
 
     {
-        //on met tout à null
+        //on met tout à null.
         $this->client = null;
 
         $this->serializer = null;
